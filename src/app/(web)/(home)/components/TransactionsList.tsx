13c22449149b723a81a07b08099c5c56bfecfc0c
jpedro002 <joaopsilvavolei@gmail.com>
2024-02-11 16:40:31 -0300

    fix transaction list component he didn't have using the createdAd and category

--- src/app/(web)/(home)/components/TransactionsList.tsx
+++ src/app/(web)/(home)/components/TransactionsList.tsx
@@ -6,38 +6,41 @@ import clsx from 'clsx'
 
 export const TransactionsList = () => {
   const { transactions } = useTransactions()
+  const dateFormatter = new Intl.DateTimeFormat('pt-BR')
 
   return (
     <table className="max-w-[70rem] mx-auto flex flex-col mt-6 gap-2 ">
-      {transactions.map((transaction) => (
-        <tr
-          key={transaction.id}
-          className="bg-gray-personalized-gray3 w-full flex gap-2 py-5 px-8
-            rounded-lg
-            "
-        >
-          <td className="w-full text-gray-personalized-gray6 text-base">
-            {transaction.description}
-          </td>
-          <td
-            className={clsx(
-              `min-w-[12.5rem] text-base `,
-              transaction.transactionType === 'entrada'
-                ? ' text-green-light'
-                : 'text-red-default',
-            )}
+      <tbody className="space-y-2">
+        {transactions.map((transaction) => (
+          <tr
+            key={transaction.id}
+            className="bg-gray-personalized-gray3 w-full flex gap-2 py-5 px-8
+              rounded-lg
+              "
           >
-            {transaction.transactionType === 'entrada' ? '' : '- '}
-            {priceFormatter.format(transaction.price)}
-          </td>
-          <td className="min-w-[15rem]   text-gray-personalized-gray6 text-base      ">
-            Venda
-          </td>
-          <td className="min-w-[5.75rem] text-gray-personalized-gray6 text-base        ">
-            13/04/2022
-          </td>
-        </tr>
-      ))}
+            <td className="w-full text-gray-personalized-gray6 text-base">
+              {transaction.description}
+            </td>
+            <td
+              className={clsx(
+                `min-w-[12.5rem] text-base `,
+                transaction.transactionType === 'entrada'
+                  ? ' text-green-light'
+                  : 'text-red-default',
+              )}
+            >
+              {transaction.transactionType === 'entrada' ? '' : '- '}
+              {priceFormatter.format(transaction.price)}
+            </td>
+            <td className="min-w-[15rem]   text-gray-personalized-gray6 text-base      ">
+              {transaction.category}
+            </td>
+            <td className="min-w-[5.75rem] text-gray-personalized-gray6 text-base        ">
+              {dateFormatter.format(new Date(transaction.createdAt))}
+            </td>
+          </tr>
+        ))}
+      </tbody>
     </table>
   )
 }
