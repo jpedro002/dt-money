534b9919594228f3ba13780f9603bb09efae23ee
jpedro002 <joaopsilvavolei@gmail.com>
2024-02-13 18:55:24 -0300

    fix: table estructure

--- src/app/(web)/(home)/components/TransactionsList.tsx
+++ src/app/(web)/(home)/components/TransactionsList.tsx
@@ -10,10 +10,10 @@ export const TransactionsList = () => {
   const dateFormatter = new Intl.DateTimeFormat('pt-BR')
 
   return (
-    <table className="max-w-[70rem] mx-auto flex flex-col mt-6 gap-2 px-4 lg:px-0 pb-8  ">
-      <tbody className="flex justify-evenly flex-wrap gap-4">
+    <div className="max-w-[70rem] mx-auto flex flex-col mt-6 gap-2 px-4 lg:px-0 pb-8  ">
+      <ul className="flex justify-evenly flex-wrap gap-4">
         {transactions.map((transaction) => (
-          <tr
+          <li
             key={transaction.id}
             className="bg-gray-personalized-gray3
             min-w-[326px] w-full  flex flex-col p-5 rounded-lg
@@ -21,10 +21,10 @@ export const TransactionsList = () => {
             md:py-5 md:px-8 md:rounded-lg
               "
           >
-            <td className="w-full text-gray-personalized-gray6 text-base">
+            <span className="w-full text-gray-personalized-gray6 text-base">
               {transaction.description}
-            </td>
-            <td
+            </span>
+            <span
               className={clsx(
                 `min-w-[12.5rem] text-base font-bold `,
                 transaction.transactionType === 'entrada'
@@ -34,20 +34,20 @@ export const TransactionsList = () => {
             >
               {transaction.transactionType === 'entrada' ? '' : '- '}
               {priceFormatter.format(transaction.price)}
-            </td>
-            <div className="flex justify-between md:gap-2 md:justify-start">
-              <td className="w-full md:min-w-[15rem] text-gray-personalized-gray5 text-base flex flex-row items-center     ">
+            </span>
+            <span className="flex justify-between md:gap-2 md:justify-start">
+              <div className="w-full md:min-w-[15rem] text-gray-personalized-gray5 text-base flex flex-row items-center     ">
                 <ArrowBigRight className="md:hidden mr-0.5" size={16} />
                 {transaction.category}
-              </td>
-              <td className="sm:min-w-[5.75rem] min-w-[6.5rem] text-gray-personalized-gray5 text-base flex flex-row items-center       ">
+              </div>
+              <div className="sm:min-w-[5.75rem] min-w-[6.5rem] text-gray-personalized-gray5 text-base flex flex-row items-center       ">
                 <CalendarDays className="md:hidden mr-0.5" size={16} />
                 {dateFormatter.format(new Date(transaction.createdAt))}
-              </td>
-            </div>
-          </tr>
+              </div>
+            </span>
+          </li>
         ))}
-      </tbody>
-    </table>
+      </ul>
+    </div>
   )
 }
