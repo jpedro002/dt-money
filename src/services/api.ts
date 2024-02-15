38a166f39964a6279986ac13ca0647d40baf4873
jpedro002 <joaopsilvavolei@gmail.com>
2024-02-15 14:30:35 -0300

    Update API base URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { ENV } from '@/env'
 
 export const api = (path: string, unit?: RequestInit) => {
-  const baseUrl = ENV.NEXT_PUBLIC_API_URL
+  const baseUrl = process.env.NEXT_PUBLIC_API_URL
   const apiPrefix = '/api'
   const url = new URL(apiPrefix.concat(path), baseUrl)
 
