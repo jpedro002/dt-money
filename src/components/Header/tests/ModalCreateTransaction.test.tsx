4b39f6ce8c1c9843a211866276d96f53a6dc7798
jpedro002 <joaopsilvavolei@gmail.com>
2024-03-04 13:10:40 -0300

    wip

--- src/components/Header/tests/ModalCreateTransaction.test.tsx
+++ src/components/Header/tests/ModalCreateTransaction.test.tsx
@@ -1,100 +1,65 @@
-import { fireEvent, render } from '@testing-library/react'
-import '@testing-library/jest-dom'
+// ModalCreateTransaction.test.tsx
+import React from 'react'
+import { render, fireEvent } from '@testing-library/react'
+import { act } from 'react-dom/test-utils'
 import { ModalCreateTransaction } from '../ModalCreateTransaction'
 import {
   TransactionsContext,
   TransactionsContextData,
+  useTransactions,
 } from '@/contexts/transactionsContexts'
 
-const handleAddTransaction = vi.fn()
-const modalIsOpen = false
-const changeModalState = vi.fn()
-const itemToEdit = null
-const handleUpdateTransaction = vi.fn()
+vi.mock('@/contexts/transactionsContexts', async (importOriginal) => {
+  const mod =
+    await importOriginal<typeof import('@/contexts/transactionsContexts')>()
+  return {
+    ...mod,
+    useTransactions: vi.fn(() => ({
+      handleAddTransaction: vi.fn(),
+      modalIsOpen: true,
+      changeModalState: vi.fn(),
+      itemToEdit: null,
+      handleUpdateTransaction: vi.fn(),
+    })),
+  }
+})
 
-const renderComponent = () => {
+const renderModalCreateTransaction = () => {
   return render(
     <TransactionsContext.Provider
-      value={
-        {
-          handleAddTransaction,
-          modalIsOpen,
-          changeModalState,
-          itemToEdit,
-          handleUpdateTransaction,
-        } as unknown as TransactionsContextData
-      }
+      value={{} as unknown as TransactionsContextData}
     >
       <ModalCreateTransaction />
     </TransactionsContext.Provider>,
   )
 }
 
-test('renders a message', () => {
-  const { getByText } = renderComponent()
+test('calls handleAddTransaction with empty places', async () => {
+  const { getByTestId } = renderModalCreateTransaction()
+  const form = getByTestId('form')
 
-  expect(getByText('Nova transação')).toBeInTheDocument()
+  await act(async () => {
+    fireEvent.submit(form)
+  })
+
+  expect(useTransactions().handleAddTransaction).not.toHaveBeenCalled()
 })
-
-test('open modal when button is clicked', () => {
-  const { getByText } = renderComponent()
-
-  const button = getByText('Nova transação')
-  fireEvent.click(button)
-
-  expect(changeModalState).toHaveBeenCalled()
-})
-
-test('handles adding a transaction', () => {
-  // const mockSetCurrentTransaction = {
-  //   setCurrentTransaction: (transaction: 'entrada' | 'saida') => {},
-  // }
-
-  // const spyCurrentTransaction = vi.spyOn(
-  //   mockSetCurrentTransaction,
-  //   'setCurrentTransaction',
-  // )
-
-  const { getByText, getByPlaceholderText } = render(
-    <TransactionsContext.Provider
-      value={
-        {
-          handleAddTransaction,
-          modalIsOpen: true,
-          changeModalState,
-          itemToEdit,
-          handleUpdateTransaction,
-        } as unknown as TransactionsContextData
-      }
-    >
-      <ModalCreateTransaction />
-    </TransactionsContext.Provider>,
-  )
-
+test('calls handleAddTransaction with right places', async () => {
+  const { getByTestId, getByPlaceholderText, getByText } =
+    renderModalCreateTransaction()
+  const form = getByTestId('form')
   const amountInput = getByPlaceholderText('Preço')
-  fireEvent.change(amountInput, { target: { value: '10' } })
-
   const descriptionInput = getByPlaceholderText('Descrição')
-  fireEvent.change(descriptionInput, { target: { value: 'Test transaction' } })
-
   const categoryInput = getByPlaceholderText('Categoria')
-  fireEvent.change(categoryInput, { target: { value: 'Test category' } })
 
-  const buttonIncome = getByText('Entrada')
-  fireEvent.click(buttonIncome)
+  await act(async () => {
+    fireEvent.change(amountInput, { target: { value: '10' } })
+    fireEvent.change(descriptionInput, {
+      target: { value: 'Test transaction' },
+    })
+    fireEvent.change(categoryInput, { target: { value: 'Test category' } })
+    fireEvent.submit(form)
+  })
 
-  // expect(spyCurrentTransaction).toHaveBeenCalledWith('entrada')
-
-  // const submitButton = getByText('Cadastrar')
-  // fireEvent.click(submitButton)
-
-  // const form = getByTestId('form')
-  // fireEvent.submit(form)
-
-  // expect(handleSubmit).toHaveBeenCalledWith({
-  //   description: 'Test transaction',
-  //   price: 10,
-  //   category: 'Test category',
-  //   transactionType: 'entrada',
-  // })
+  // expect(useTransactions().handleAddTransaction).toHaveBeenCalled()
 })
